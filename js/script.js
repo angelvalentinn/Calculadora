714662d3cff48d29edc1a909698dd938146af4e2
angelvalentinn <altieriangel1@gmail.com>
2023-06-15 20:05:45 -0300

    cambiando el output hecho con un p por un input de solo lectura

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const botones = document.querySelectorAll(".btn");
 const screen = document.getElementById("screen");
 
-if(screen.innerText.length == 0) screen.innerText = "0"; //cuando el contenido de la pantalla tenga una 
+if(screen.value.length == 0) screen.value = "0"; //cuando el contenido de la pantalla tenga una 
 //longitud de 0 la remplazamos por un "0", asi siempre se va a ver un 0 en la pantalla y no queda vacío
 
 botones.forEach(btn => { 
@@ -11,31 +11,31 @@ botones.forEach(btn => {
 
         if(valorBtn === "=") {
             try {   //Si el btn presionado es el "=" tratamos de resolverlo si no se puede capturamos el error y mostramos un mensaje
-                screen.innerText = eval(screen.innerText); //eval es un método del objeto global window que recibe como parámetro un string y lo resuelve como código js
+                screen.value = eval(screen.value); //eval es un método del objeto global window que recibe como parámetro un string y lo resuelve como código js
                 return; //retornamos siempre al presionar un boton ya que  queremos que ejecute solo la función del operador
             } catch {
-                screen.innerText = "Error..."
+                screen.value = "Error..."
                 return;
             }
         }
 
         if (valorBtn === "AC") {
-            screen.innerText = "0"; //Operador para borrar todo 
+            screen.value = "0"; //Operador para borrar todo 
             return;
         }
 
         if(valorBtn === "DEL") { //Operador que borra solo el ultimo número
-            if(screen.innerText.length == 1 || screen.innerText === "Error...") {
-                screen.innerText = "0"; //Si la longitud de los numeros que hay en la pantalla es de 1 y borramos pone un "0", asi no queda vacío    
+            if(screen.value.length == 1 || screen.value === "Error...") {
+                screen.value = "0"; //Si la longitud de los numeros que hay en la pantalla es de 1 o dice "Error... "y borramos pone un "0", asi no queda vacío    
                 return;
             } else {
-                screen.innerText = screen.innerText.slice(0,-1); //Recorremos el contenido de texto de la pantalla y devolvemos un array sin el ultimo elemento, -1 indica el ultimo elemento que es excluido
+                screen.value = screen.value.slice(0,-1); //Recorremos el contenido de texto de la pantalla y devolvemos un array sin el ultimo elemento, -1 indica el ultimo elemento que es excluido
                 return;
             }
         }
 
-        screen.innerText === "0" || screen.innerText === "Error..." ? screen.innerText = valorBtn : screen.innerText += valorBtn;
-        //Si el contenido de la pantalla es 0 y presionamos queremos que se sobreescriba asi no queda el 0 adelante de todo
+        screen.value === "0" || screen.value === "Error..." ? screen.value = valorBtn : screen.value += valorBtn;
+        //Si el contenido de la pantalla es 0 o "Error..." y presionamos queremos que se sobreescriba asi no queda el 0 adelante de todo
         //sino que se acumulen los numeros
     })
 })
